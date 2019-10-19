92924f199bbdc72ef90e432c19b3a817015c134d
federicomichela <federico.michela@gmail.com>
2019-10-19 18:23:33 +0100

    INSTRUCTIONS: Development Strategy (9/10) - ensure the feed container has at least one entry after loadFeed is executed

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -81,14 +81,23 @@ $(function() {
     });
 
     /* TODO: Write a new test suite named "Initial Entries" */
-
+    describe('Initial Entries', () => {
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         let feedContainer = document.querySelector('.feed');
 
+         feedContainer.innerHTML = '';
+
+         beforeEach(done => loadFeed(0, done));
+
+         it('at least one feed listed after loadFeed execution', () => {
+            expect(feedContainer.childElementCount).not.toBe(0);
+         });
+     });
     /* TODO: Write a new test suite named "New Feed Selection" */
 
         /* TODO: Write a test that ensures when a new feed is loaded
